5e9eeb9c0acd8badbacb84ce74bfb113fd2ae546
BisiDavi <oludavidconnect@gmail.com>
2022-10-27 20:12:48 +0100

    zoom added

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const googleCalendar = require("./api/gc");
 
 const app = express();
 
-app.use(express.json({ extended: false }));
-app.use("/api/gc", googleCalendar);
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello");
 });
+
+app.use("/api/gc", googleCalendar);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
