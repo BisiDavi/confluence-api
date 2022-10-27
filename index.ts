1204df41beb9e1a0921dc3895792e7f178bf1b46
BisiDavi <oludavidconnect@gmail.com>
2022-10-27 11:34:27 +0100

    server updated

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+
+// app.use("/api", "Hello world");
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
