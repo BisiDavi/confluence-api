d2183da719187f66186ba762ddab7c4a4313c850
BisiDavi <oludavidconnect@gmail.com>
2022-10-29 16:12:34 +0100

    gmail api added

---
+++ api/gmail.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const router = express.Router();
+
+router.post("/send-meeting-email-notification", (req, res) => {
+  async function sendEmail() {
+    const mailOptions = {
+      from: "oludavidconnect@gmail.com",
+      to: req.body.receipent,
+      subject: `This is to notify you about the upcoming meeting - ${req.body.title}`,
+    };
+    try {
+      const transport = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: "oludavidconnect@gmail.com",
+          clientID: process.env.GMAIL_CLIENT_ID,
+          clientSecret: process.env.GMAIL_CLIENT_SECRET,
+          accessToken,
+        },
+      });
+    } catch (error) {
+      console.log("send-email-error", error);
+    }
+  }
+});
